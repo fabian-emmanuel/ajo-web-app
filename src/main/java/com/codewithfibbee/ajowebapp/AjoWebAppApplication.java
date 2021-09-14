b370e1a5795ce4cdf9e80d0671dd230de35daf1c
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-09-14 11:20:45 +0100

    bootstrapping the ajo web app

---
+++ src/main/java/com/codewithfibbee/ajowebapp/AjoWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.codewithfibbee.ajowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjoWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjoWebAppApplication.class, args);
+    }
+
+}
